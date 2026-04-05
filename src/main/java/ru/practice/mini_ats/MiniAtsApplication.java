cccb772ef95f80431569cffcaa0d2eb1d8c5127d
Vladimir <besshaposhnikovv@gmail.com>
2026-04-05 18:20:06 +0300

    chore: init Spring boot project

---
+++ src/main/java/ru/practice/mini_ats/MiniAtsApplication.java
@@ -0,0 +1,13 @@
+package ru.practice.mini_ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAtsApplication.class, args);
+	}
+
+}
